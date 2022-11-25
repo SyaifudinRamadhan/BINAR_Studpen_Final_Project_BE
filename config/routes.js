ad15d39b6ed50d47f533572501ce54b3ea6516a0
gotham23 <fikrifahresi3@gmail.com>
2022-11-25 19:36:31 +0700

    menambahkan routes create data carts

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,9 @@
-const express = require("express")
-const ctrl = require("../app/controllers")
+const express = require('express');
+const ctrl = require('../app/controllers');
 const path = require('path');
-const { uploadUser } = require("../app/controllers/middleware/upload");
+const { uploadUser } = require('../app/controllers/middleware/upload');
 
-
-const apiRouter = express.Router()
+const apiRouter = express.Router();
 
 apiRouter.use(express.static(path.join(__dirname, '../bin/public')));
 /**
@@ -12,29 +11,30 @@ apiRouter.use(express.static(path.join(__dirname, '../bin/public')));
  *       implementations
  */
 // Route authentikasi
-apiRouter.post("/api/v1/login", ctrl.middleware.login, ctrl.api.v1.login)
-apiRouter.post("/api/v1/register", ctrl.middleware.register, ctrl.api.v1.register)
-apiRouter.post("/api/v1/loginRegGoogle", ctrl.api.v1.loginRegGoogle)
+apiRouter.post('/api/v1/login', ctrl.middleware.login, ctrl.api.v1.login);
+apiRouter.post('/api/v1/register', ctrl.middleware.register, ctrl.api.v1.register);
+apiRouter.post('/api/v1/loginRegGoogle', ctrl.api.v1.loginRegGoogle);
 // Route untuk umum
 
 // Route untuk user login
-apiRouter.get("/api/v1/who-am-i", ctrl.middleware.isLogin, ctrl.api.v1.whoAmI)
-apiRouter.put("/api/v1/update-profile", ctrl.middleware.isLogin, uploadUser.single('image'), ctrl.middleware.uploadHandler, ctrl.middleware.updateUser, ctrl.api.v1.updateProfile)
+apiRouter.get('/api/v1/who-am-i', ctrl.middleware.isLogin, ctrl.api.v1.whoAmI);
+apiRouter.put('/api/v1/update-profile', ctrl.middleware.isLogin, uploadUser.single('image'), ctrl.middleware.uploadHandler, ctrl.middleware.updateUser, ctrl.api.v1.updateProfile);
+
+// Route carts
+apiRouter.post('/api/v1/carts', controllers.api.v1.carts.create);
 apiRouter.get('/api/v1/carts', ctrl.api.v1.carts.list);
 // Route untuk admin
-apiRouter.delete("/api/v1/delete-user", ctrl.middleware.isLogin, ctrl.middleware.isAdmin, ctrl.middleware.getUser, ctrl.api.v1.deleteUser)
+apiRouter.delete('/api/v1/delete-user', ctrl.middleware.isLogin, ctrl.middleware.isAdmin, ctrl.middleware.getUser, ctrl.api.v1.deleteUser);
 /**
  * TODO: Delete this, this is just a demonstration of
  *       error handler
  */
 
-apiRouter.get("/api/v1/errors", () => {
-  throw new Error(
-    "The Industrial Revolution and its consequences have been a disaster for the human race."
-  )
-})
+apiRouter.get('/api/v1/errors', () => {
+  throw new Error('The Industrial Revolution and its consequences have been a disaster for the human race.');
+});
 
-apiRouter.use(ctrl.api.main.onLost)
-apiRouter.use(ctrl.api.main.onError)
+apiRouter.use(ctrl.api.main.onLost);
+apiRouter.use(ctrl.api.main.onError);
 
-module.exports = apiRouter
+module.exports = apiRouter;
