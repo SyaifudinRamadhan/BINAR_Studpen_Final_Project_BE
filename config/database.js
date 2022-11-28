0a3e0b3d4306ed88069aba4158751bbb84cf38c2
SyaifudinRamadhan <syaifudinramadhan@gmail.com>
2022-11-28 07:32:36 +0700

    Ubah DB untuk deploy

--- config/database.js
+++ config/database.js
@@ -13,28 +13,32 @@ const {
   DB_PASSWORD = process.env.DB_PASSWORD,
   DB_HOST = process.env.DB_HOST,
   DB_NAME = process.env.DB_NAME,
+  DB_PORT = process.env.DB_PORT
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
+    port: DB_PORT,
     dialect: "postgres",
   },
 };
